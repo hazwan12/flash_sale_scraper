dc6e86f33572b6871374281cf94e9ff0a32e7bf0
Hazwan <hazzwan12@gmail.com>
2021-08-20 22:43:20 +0800

    Telegram Bot v1.0.0 1) /start welcome message and features 2) /search bot engages in a conversation and search keyword provied by user

--- src/bot/telegram.py
+++ src/bot/telegram.py
@@ -1,25 +1,29 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
+import os
+import logging
+from dotenv import load_dotenv
 
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+
+from ..sql import crud
 from ..sql.database import SessionLocal
 
-BOT_TOKEN = '1999877100:AAHj7H5m2feACSe6gJ4KxTF00ytw8wOfrtY'
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-updater = Updater(token=BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+logger = logging.getLogger(__name__)
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+load_dotenv()
 
-def start(update, context):
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+KEYWORD = range(1)
+
+def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     username = update["message"]["chat"]["username"]
+
+    logger.info("User %s started bot", username)
 
     start_message = """
     Welcome to the Flash Sales Bot {}
@@ -27,16 +31,68 @@ def start(update, context):
      - To search items on sale enter /search
      - To list all your reminders enter /list_reminders
      - To report an issue enter /report
+     - To stop this chat /cancel
     """.format(username)
 
     context.bot.send_message(chat_id=chat_id, text=start_message)
 
-def search():
+def search(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    logger.info("User %s initiated search", user.username)
+    update.message.reply_text("Please provide a keyword of the item you want to search")
+
+    return KEYWORD
+
+def keyword(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    logger.info("User %s is searching for %s", user.username, update.message.text)
+    items = crud.get_item(SessionLocal(), update.message.text)
+
+    update.message.reply_text("The following items match the keyword you are looking for:")
+
+    for item in items:
+        update.message.reply_text("""
+            Item Name : {}
+            Item Original Price : {}
+            Item Discount Price : {}
+            Item Sale Time : {}
+            Item Link : {}
+        """.format(item.item_name, item.item_original_price, item.item_discount_price, item.item_sale_time, item.item_url,))
+
+def remind(update: Update, context: CallbackContext):
     pass
 
-def remind():
-    pass
+def cancel(update: Update, context: CallbackContext) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text(
+        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
 
 def start_bot():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(token=BOT_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Add the start handler
     dispatcher.add_handler(CommandHandler("start", start))
+
+    # Add conversation handler
+    convo_handler = ConversationHandler(
+        entry_points=[CommandHandler('search', search)],
+        states={
+            KEYWORD: [MessageHandler(Filters.text, keyword)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+    dispatcher.add_handler(convo_handler)
+
+    # Start the Bot
     updater.start_polling()
+
+    updater.idle()
